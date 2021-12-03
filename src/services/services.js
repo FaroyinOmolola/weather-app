d37de540bb30f8e6bd4edab8ddf5dabe96323d3e
Faroyin Omolola <faroyinomolola@gmail.com>
2021-12-03 18:34:39 +0100

    ..

--- src/services/services.js
+++ src/services/services.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const API_KEY = "53b5476afbbc4acb13c7c343712f3270";
 //created axios instance
 const weatherInstance = axios.create({
-	baseURL: `http://api.openweathermap.org/data/2.5/forecast?q=Munich,de&units=metric&appid=${API_KEY}`,
+	baseURL: `https://api.openweathermap.org/data/2.5/forecast?q=Munich,de&units=metric&appid=${API_KEY}`,
 });
 //using axios instance for https call
 export const getWeather = () => {
